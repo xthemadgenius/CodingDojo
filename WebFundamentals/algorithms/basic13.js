57a8eb05e5d8cbfd4c9bd65ffc3f361a505beedd
Javier Calderon <javierjcjr@gmail.com>
2020-05-22 23:25:28 -0700

    added code

--- WebFundamentals/algorithms/basic13.js
+++ WebFundamentals/algorithms/basic13.js
@@ -17,3 +17,34 @@ function random(num){
 console.log(random(255))
 
 // problem #3
+function random(num){
+    var foo = [];
+    for (var i = 1; i <= num; i++) {
+        foo.push(i);
+    }
+    console.log(foo)
+    return foo;
+}
+var addUp = num => num.reduce((a,b) => a + b);
+var arr = random(255);
+var x = addUp(arr);
+console.log(x)
+
+// problem #4
+function random(num){
+    var foo = [];
+    for (var i = 0; i < num.length; i++) {
+        foo.push(num[i]);
+    }
+    return foo;
+}
+var x = [1,2,3,4];
+console.log(random(x));
+
+// problem #5 
+var getMax = arr => Math.max(...arr);
+var x = [1,2,3,4];
+console.log(getMax(x));
+
+// problem #6 
+
