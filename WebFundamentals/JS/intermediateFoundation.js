8837c6b714e0c3ee01a3d7eab55edc083d9adbae
Javier Calderon <javierjcjr@gmail.com>
2020-05-27 01:04:00 -0700

    now done for the night

--- WebFundamentals/JS/intermediateFoundation.js
+++ WebFundamentals/JS/intermediateFoundation.js
@@ -64,3 +64,74 @@ var doubTrop = dup => dup.reduce(function (res, current) {
     return res.concat([current, current]);
 }, []);
 console.log(doubTrop( [4, "Ulysses", 42, false]));
+
+// ***** Part 2 *********
+
+var world = [
+    [1,1,1,0,0,0,0,3,2,2],
+    [0,0,0,0,1,0,0,3,3,3],
+    [0,2,2,0,1,0,0,0,0,0],
+    [0,2,2,0,1,1,1,1,0,0],
+    [0,2,2,0,1,0,0,0,0,3],
+    [0,2,2,0,1,0,0,3,3,3],
+    [0,0,0,0,1,0,0,3,3,3]];
+
+var mapping = {
+    0: 'white',
+    1: 'red',
+    2: 'yellow',
+    3: 'blue',
+    4: 'green'
+}
+
+function drawWorld()
+{
+    var output = '';
+    for(var i=0; i<world.length; i++)
+    {
+        output += '<div class="row">';
+        for(var j=0; j<world[i].length; j++)
+        {
+            output += "<div class='"+mapping[world[i][j]]+"'></div>";
+        }
+        output += '</div>';
+    }
+    document.getElementById('container').innerHTML = output;
+    // console.log(output);
+}
+
+document.onclick = function(e)
+{
+    // console.log('Coordinate Clicked', e.x, e.y);
+
+    var x = Math.floor(e.x/50);
+    var y = Math.floor(e.y/50);
+
+    // console.log('X, Y', x, y);
+    // console.log('Original Color', world[y][x]);
+
+    console.log('Replace color', world[y][x], ' with color 4 starting from x:', x, 'y:', y);
+    fill(x, y, world[y][x], 4);
+}
+
+function fill(x,y,original_color, color)
+{
+if(y<0 || y>=world.length || x<0 || x>=10)
+return false;
+
+    if(world[y][x] != original_color)
+        return false;
+    
+    world[y][x] = color;
+    fill(x, y-1, original_color, color);
+    fill(x, y+1, original_color, color);
+    fill(x-1, y, original_color, color);
+    fill(x+1, y, original_color, color);	
+
+    drawWorld();
+    
+}
+
+drawWorld();
+
+// had some help on the second part was difficult to figure out 
