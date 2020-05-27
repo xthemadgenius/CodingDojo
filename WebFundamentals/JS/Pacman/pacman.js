bcba420f483c97fc87f482b47f3bf57f9d46392f
Javier Calderon <javierjcjr@gmail.com>
2020-05-26 21:09:49 -0700

    got pacman to move

--- WebFundamentals/JS/Pacman/pacman.js
+++ WebFundamentals/JS/Pacman/pacman.js
@@ -1,13 +1,13 @@
 var world = [
-    [2,2,2,2,2,2,2,2,2,2],
-    [2,1,1,2,1,1,1,1,1,2],
-    [2,1,1,2,1,2,2,2,1,2],
-    [2,1,1,2,1,2,1,2,1,2],
-    [2,1,1,2,1,2,1,2,1,2],
-    [2,1,1,2,2,2,1,2,1,2],
-    [2,1,1,1,1,1,1,2,1,2],
-    [2,1,1,1,1,1,1,1,1,2],
-    [2,2,2,2,2,2,2,2,2,2]
+    [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+    [2,1,1,2,1,1,1,1,1,2,1,1,2,1,2,1,1,1,1,2],
+    [2,1,2,2,1,2,2,2,1,2,1,1,2,1,1,1,2,1,2,2],
+    [2,1,1,2,1,2,1,2,1,2,1,1,1,1,1,1,2,1,1,2],
+    [2,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,2,2,1,2],
+    [2,1,1,2,2,2,1,2,1,2,1,1,1,2,2,1,2,1,1,2],
+    [2,1,2,2,1,1,1,2,1,1,1,1,2,2,1,1,2,1,2,2],
+    [2,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,2],
+    [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2]
 ];
 var score = 0;
 
@@ -45,12 +45,16 @@ displayPacman();
 displayScore();
 document.onkeydown = function(e){
     if(e.keyCode == 37 && world[pacman.y][pacman.x-1] != 2){
+        document.getElementById('pacman').style.transform = "rotate(-180deg)";
         pacman.x--;
     } else if(e.keyCode == 39 && world[pacman.y][pacman.x + 1] != 2){
+        document.getElementById('pacman').style.transform = "none";
         pacman.x++;
     }else if(e.keyCode == 38 && world[pacman.y - 1][pacman.x] != 2){
+        document.getElementById('pacman').style.transform = "rotate(-90deg)";
         pacman.y--;
     }else if(e.keyCode == 40 && world[pacman.y + 1][pacman.x] != 2){
+        document.getElementById('pacman').style.transform = "rotate(90deg)";
         pacman.y++;
     }
     if(world[pacman.y][pacman.x] == 1){
