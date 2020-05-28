cde2a87813953c6365efedebb06c5c6ff7e602bc
Javier Calderon <javierjcjr@gmail.com>
2020-05-28 14:46:06 -0700

    finished debugJQuery

--- WebFundamentals/jQuery/DebuggingJquery/README.md
+++ WebFundamentals/jQuery/DebuggingJquery/README.md
@@ -2,3 +2,17 @@ Debugging-jQuery
 ================
 
 Clone this and correct the jQuery errors!
+
+
+Just Corrected the Code 
+Has a few typos and works fine now
+
+Note that I did clone the repo into my code removed the Git attached to the original and made the code my own
+using 
+if first removed the repo from my main Repo Coding Dojo
+
+then I removed git files in the repo using 
+rm -rf .git 
+
+and then I renamed the Repo Folder
+added it back into my main Repo and here we are with a completed assignment
--- WebFundamentals/jQuery/DebuggingJquery/main.js
+++ WebFundamentals/jQuery/DebuggingJquery/main.js
@@ -1,22 +1,24 @@
-$('p, h3, li').hoverfunction(){
-    $(this).css('color', 'white');
-}, function(){
-    $(this).css('color', 'black');
-});
+$(document).ready(function(){
+    $('p, h3, li').hover(function(){
+        $(this).css('color', 'white');
+    }, function(){
+        $(this).css('color', 'black');
+    });
 
-$('a').click(function(){
-    alert("we don't actually serve pizza...");
-});
+    $('a').click(function(){
+        alert("we don't actually serve pizza...");
+    });
 
-$('img').click(function(){
-    $(this).slideUp();
-);
+    $('img').click(function(){
+        $(this).slideUp();
+    });
 
-$('h5').click(function(){
-    $('img').slide Down();
-});
+    $('h5').click(function(){
+        $('img').slideDown();
+    });
 
-$('button').click(function(){
-    var result = $('input:checked').value()  
-    alert("Thank you for your input, we like " + result + " too!");
+    $('button').click(function(){
+        var result = $('input:checked').val()  
+        alert("Thank you for your input, we like " + result + " too!");
+    });
 });
