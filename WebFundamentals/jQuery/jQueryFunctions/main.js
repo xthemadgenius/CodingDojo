1d285ee6c814f7d136d3a9849efe178309e19f3d
Javier Calderon <javierjcjr@gmail.com>
2020-05-27 17:51:53 -0700

    finished proj

--- WebFundamentals/jQuery/jQueryFunctions/main.js
+++ WebFundamentals/jQuery/jQueryFunctions/main.js
@@ -1,70 +1,63 @@
-// $(document).ready(function() {
-//     // .addClass and .click
-//     $('button.addClass').click(function () {
-//         $('p.addClass').addClass('red');
-//     })
-//     // .after
-//     $('button.after').click(function() {
-//         $('p.after').after('   <i>I am now after the paragraph!</i>')
-//     })
-//     // .append
-//     $('button.append').click(function() {
-//         $('p.append').append('   <b>...I am now appended. And another thing...!</b>')
-//     })
-//     // .attr
-//     $('button.attr').click(function() {
-//         $('img.attr').attr('alt', 'cat');
-//     })
-//     // .before
-//     $('button.before').click(function() {
-//         $('p.before').before('<i>I am now before the paragraph!</i>   ')
-//     })
-//     // .data
-//     $('button.data1').click(function() {
-//         $('div.data').data('greeting', 'Hello World!');
-//     })
-//     $('button.data2').click(function() {
-//         alert($('div.data').data('greeting'));
-//     })
-//     // .fadeIn and .fadeOut
-//     $('button.fadeout').click(function() {
-//         $('p.fadeTarget').fadeOut(1800);
-//     })
-//     $('button.fadein').click(function() {
-//         $('p.fadeTarget').fadeIn(1800);
-//     })
-//     // .hide and .show
-//     $('button.hide').click(function() {
-//         $('p.hideShowTarget').hide();
-//     })
-//     $('button.show').click(function() {
-//         $('p.hideShowTarget').show();
-//     })
-//     // .html 
-//     $('button.html').click(function(){
-//         $('p.html').html('<b>I am <i>brand new</i> text</b>')
-//     })
-//     // .slideDown and .slideUp
-//     $('button.slideUp').click(function() {
-//         $('p.slideTarget').slideUp();
-//     })
-//     $('button.slideDown').click(function() {
-//         $('p.slideTarget').slideDown();
-//     })
-//     // .slideToggle
-//     $('button.slideToggle').click(function() {
-//         $('#shrug').slideToggle('slow');
-//     })
-//     // .text
-//     $('button.text').click(function() {
-//         $('p.text').text('I am brand new text!');
-//     })
-//     // .toggle
-//     $('button.toggle').click(function() {
-//         $('p.toggle').toggle();
-//     })
-//     // .val
-//     $('button.val').click(function() {
-//         $('input:text').val('Tennessee Tuxedo');
-//     })
-// })
+$(document).ready(function() {
+    // .addClass and .click
+    $('button.addClass').click(function () {
+        $('p.addClass').addClass('red');
+    })
+    // .hide and .show
+    $('button.hide').click(function() {
+        $('p.hideShow').hide();
+    })
+    $('button.show').click(function() {
+        $('p.hideShow').show();
+    })
+    // .toggle
+    $('button.toggle').click(function() {
+        $('p.toggle').toggle();
+    })
+    // .slideDown and .slideUp
+    $('button.slideUp').click(function() {
+        $('p.slideUpDown').slideUp();
+    })
+    $('button.slideDown').click(function() {
+        $('p.slideUpDown').slideDown();
+    })
+    // .slideToggle
+    $('button.slideToggle').click(function() {
+        $('#sToggle').slideToggle('slow');
+    })
+    // .fadeIn and .fadeOut
+    $('button.fadeOut').click(function() {
+        $('p.fadeInOut').fadeOut(1800);
+    })
+    $('button.fadeIn').click(function() {
+        $('p.fadeInOut').fadeIn(1800);
+    })
+    // .before
+    $('button.before').click(function() {
+        $('p.before').before('<i>I dont hate because youre fat, youre fat because I hate you....</i>\n')
+    })
+    // .after
+    $('button.after').click(function() {
+        $('p.after').after('\n<i>I AM IRON MAN!!!</i>\n')
+    })
+    // .append
+    $('button.append').click(function() {
+        $('p.append').append('\n<b>I dont code I google things!!!!!!!</b>\n')
+    })
+    // .html 
+    $('button.html').click(function(){
+        $('p.html').html('<b>I am <i>all the Code</i> that you need</b>')
+    })
+    // .attr
+    $('button.attr').click(function() {
+        $('img.attr').attr('alt', 'Bob-Ross');
+    })
+    // .val
+    $('button.val').click(function() {
+        $('input:text').val('Geeks Rule in Las Vegas');
+    })
+    // .text
+    $('button.text').click(function() {
+        $('p.text').text('I promise babe, I changed');
+    })
+});
