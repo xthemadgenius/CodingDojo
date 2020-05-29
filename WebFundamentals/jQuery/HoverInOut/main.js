d9975f3940343d527b983e38a92d8455b46c40f6
Javier Calderon <javierjcjr@gmail.com>
2020-05-28 21:34:40 -0700

    finished another one

--- WebFundamentals/jQuery/HoverInOut/main.js
+++ WebFundamentals/jQuery/HoverInOut/main.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-    
+    var delay=1000, setTimeoutConst;
+    var changeImg = function(){
+        var $this = $(this);
+        var funnyImg = $this.data('alt-src');
+        $this.data('alt-src', $this.attr('src'));
+        $this.attr('src', funnyImg);
+    };
+    $(function(){
+        $('img.change').hover(changeImg, changeImg);
+        clearTimeout(setTimeoutConst);
+    });
 });
