f746902574cbe7a56755abcdb159bb7caee3c21b
Javier Calderon <javierjcjr@gmail.com>
2020-06-02 01:56:24 -0700

    more code

---
+++ Python/PythonFundamentals/fundamentals/function_basics2.py
@@ -0,0 +1,9 @@
+# 1 countdown
+def countdown(num):
+    new = reversed(list(range(0, num)))
+    return new
+print(countdown(5))
+# 2 print and return
+# 3 first plus length
+# values greater than Second
+# this length, that value
