647d5501c2b53dcddd964bc7e63097e2a4cdcd0e
Javier Calderon <javierjcjr@gmail.com>
2020-06-02 10:14:02 -0700

    more Code

--- Python/PythonFundamentals/fundamentals/for_loops_basics2.py
+++ Python/PythonFundamentals/fundamentals/for_loops_basics2.py
@@ -4,6 +4,7 @@ def biggie_size(arr):
         if arr[i] > 0:
             arr[i] = "big"
     return arr
+print(biggie_size([1,-4,-5,3]))
 
 #2 Count Positives
 def count_positives(arr):
@@ -32,7 +33,46 @@ def average(arr):
 print(average([1,6,4,5,7]))
 
 #5 Length
+def length(arr):
+    return len(arr)
+print(length([1,6,4,5,7]))
+
 #6 Minimum
+def minimum(arr):
+    min = arr[0]
+    if len(arr) == 0:
+        return False
+    for i in arr:
+        if i < min:
+            min = i
+    return min
+print(minimum([1,6,4,5,7]))
+
 #7 Maximum
+def maximum(arr):
+    max = arr[0]
+    if len(arr) == 0:
+        return False
+    for i in arr:
+        if i > max:
+            max = i
+    return max
+print(maximum([37,2,1,-9]))
 #8 Ultimate Analysis
+def ultimate_analyze(arr):
+    analysis = {
+        'sumtotal' : sum_total(arr),
+        'avg' : average(arr),
+        'min' : minimum(arr),
+        'max' : maximum(arr),
+        'len' : length(arr)
+    }
+    return analysis
+print(ultimate_analyze([37,2,1,-9]))
+
+
 #9 Reverse List
+def reversed(arr):
+    arr = arr[::-1]
+    return arr
+print(reversed([1,6,4,5,7]))
--- Python/PythonFundamentals/fundamentals/functions_intermediate1.py
+++ Python/PythonFundamentals/fundamentals/functions_intermediate1.py
@@ -1,5 +1,10 @@
-import random
-def randomInt(min=10 ,max=50):
-    num = (random.random() + min) * max
-    return num
-print(randomInt())
+def randInt(min=0, max=50):
+    import random
+    num = random.random()*(max-min) + min
+    return(int(num))
+
+
+print(randInt())
+print(randInt(min=50, max=50))
+print(randInt(min=50, max=100))
+print(randInt(min=50, max=500))
