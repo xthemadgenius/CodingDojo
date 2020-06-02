780521b411939a7ee4f1a7b9ef40d0d04d92fdc3
Javier Calderon <javierjcjr@gmail.com>
2020-06-02 16:44:13 -0700

    more code

---
+++ Python/PythonFundamentals/fundamentals/insert_sort.py
@@ -0,0 +1,7 @@
+def insert_sort(arr):
+    for i in range(1, len(arr)):
+        for j in range(i-1, -1, -1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+print(insert_sort([5,1,7,23,66,90,34]))
