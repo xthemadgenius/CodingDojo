b4dc4ff9578c261d306d0c715f03eac8b049e638
Javier Calderon <javierjcjr@gmail.com>
2020-06-03 13:51:47 -0700

    more COde

---
+++ Python/PythonFundamentals/OOP/UsersWithBankAccounts/BankAccount.py
@@ -0,0 +1,31 @@
+class BankAccount:
+
+    def __init__(self, name, balance, int_rate):
+        self.name = name
+        self.balance =balance
+        self.int_rate = int_rate
+    
+    def deposit(self, amount):
+        self.balance += amount
+        return self
+
+    def withdraw(self, amount):
+        self.balance -= amount
+        return self
+
+    def display_account_info(self):
+        print(f"{self.name}, Your Balance is {self.balance} with an Interest Rate {self.int_rate}")
+
+    def yield_interest(self):
+        self.balance += (self.balance *self.int_rate)
+
+b1 = BankAccount('Obi Ezeakachi', 5000, .05)
+b2 = BankAccount('Tasha St.Patrick', 80000, .09)
+
+print(b1.name)
+b1.deposit(400).deposit(600).deposit(101).withdraw(204).yield_interest()
+print(b1.balance)
+
+print(b2.name)
+b2.deposit(3000).deposit(6500).withdraw(24).withdraw(80).withdraw(103).withdraw(22).yield_interest()
+print(b2.balance)
