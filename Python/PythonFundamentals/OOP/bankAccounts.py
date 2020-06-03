02b486b9d78422fecf82c6e95bdd23808fd6d985
Javier Calderon <javierjcjr@gmail.com>
2020-06-03 02:32:09 -0700

    fxed the code

--- Python/PythonFundamentals/OOP/bankAccounts.py
+++ Python/PythonFundamentals/OOP/bankAccounts.py
@@ -1,19 +1,28 @@
 class BankAccount:
-	def __init__(self, int_rate=0, balance=0):
-        self.int_rate = int_rate
-        self.balance = balance
-        self.name = ""
 
-	def deposit(self, amount):
+    def __init__(self, name, balance, int_rate):
+        self.name = name
+        self.balance =balance
+        self.int_rate = int_rate
+    
+    def deposit(self, amount):
         self.balance += amount
         return self
 
-	def withdraw(self, amount):
+    def withdraw(self, amount):
         self.balance -= amount
+        return self
 
-	def display_account_info(self):
-        info = self.int_rate, self.balance, self.name
-        return info
+    def display_account_info(self):
+        info = self.int_rate, self.balance, self.int_rate
+        return self
 
-	def yield_interest(self):
+    def yield_interest(self):
         self.balance += (self.balance *self.int_rate)
+
+b1 = BankAccount('Obi Ezeakachi', 5000, 1111)
+b2 = BankAccount('Tasha St.Patrick', 80000, 2222)
+b3 = BankAccount('Tommy Egan', 7000, 3333)
+print(b1.name)
+print(b2.name)
+print(b3.name)
