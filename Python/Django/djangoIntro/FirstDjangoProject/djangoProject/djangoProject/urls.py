c04d3c589fc022baea4b8194cc936c22b95cb363
Javier Calderon <javierjcjr@gmail.com>
2020-06-04 16:32:48 -0700

    server works

--- Python/Django/djangoIntro/FirstDjangoProject/djangoProject/djangoProject/urls.py
+++ Python/Django/djangoIntro/FirstDjangoProject/djangoProject/djangoProject/urls.py
@@ -13,9 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include           # import include
+    # from django.contrib import admin              # comment out, or just delete
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include('firstProj.urls')),	   
+    # path('admin/', admin.sites.urls)         # comment out, or just delete
 ]
