0f695e1a5c650914acf84cf546edaa2d79ae935d
Javier Calderon <javierjcjr@gmail.com>
2020-06-04 17:39:55 -0700

    more code

--- Python/Django/djangoIntro/FirstDjangoProject/djangoProject/firstProj/urls.py
+++ Python/Django/djangoIntro/FirstDjangoProject/djangoProject/firstProj/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path     
 from . import views
+
 urlpatterns = [
-    path('', views.index),	   
+    path('', views.index),
+    path('new', views.new),
+    path('new/<int:ranNumbers>', views.randomNum),
+    path('create', views.changeWays),  
 ]
--- Python/Django/djangoIntro/FirstDjangoProject/djangoProject/firstProj/views.py
+++ Python/Django/djangoIntro/FirstDjangoProject/djangoProject/firstProj/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render, HttpResponse
+
 def index(request):
-    return HttpResponse("this is the equivalent of @app.route('/')!")
+    return HttpResponse("placeholder to later display a list of all blogs")
+
+def new(request):
+    word = "new page,\n new me,\n whats up bro"
+    print(word)
+    return HttpResponse(word)
+
+def randomNum(request, ranNumbers):
+    return HttpResponse(f'I have {ranNumbers} in my list!')
+
+def changeWays(request):
+    print('Im starting an new page!')
+    return redirect('/')
