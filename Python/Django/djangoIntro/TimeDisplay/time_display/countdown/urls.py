25ddedb9fef5e460f3c559a74f51cd1e7c67b8f2
Javier Calderon <javierjcjr@gmail.com>
2020-06-04 22:09:39 -0700

    more code

--- Python/Django/djangoIntro/TimeDisplay/time_display/countdown/urls.py
+++ Python/Django/djangoIntro/TimeDisplay/time_display/countdown/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('timeDisplay', views.timeDisplay)	   
+    path('', views.index),	   
 ]
--- Python/Django/djangoIntro/TimeDisplay/time_display/countdown/views.py
+++ Python/Django/djangoIntro/TimeDisplay/time_display/countdown/views.py
@@ -1,8 +1,12 @@
+from __future__ import unicode_literals
+from datetime import datetime
 from django.shortcuts import render, HttpResponse
 
 def index(request):
-    return HttpResponse("this is the equivalent of @app.route('/')!")
-
-def timeDisplay(request):
-    print("\nits time to tell time\n")
-    return render(request, 'time_display.html')
+    print('*'*80)
+    now = datetime.today()
+    context = {
+        "date": now.strftime('%b %d, %Y'),
+        "time": now.strftime('%I:%M %p')
+    }
+    return render(request, 'time_display.html', context)
