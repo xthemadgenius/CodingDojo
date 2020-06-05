8a1f1c5352c66640e936b4ecfdd92167ce04a97a
Javier Calderon <javierjcjr@gmail.com>
2020-06-04 21:51:32 -0700

    more code

--- Python/Django/djangoIntro/TimeDisplay/time_display/countdown/views.py
+++ Python/Django/djangoIntro/TimeDisplay/time_display/countdown/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render, HttpResponse
+
 def index(request):
     return HttpResponse("this is the equivalent of @app.route('/')!")
+
+def timeDisplay(request):
+    print("\nits time to tell time\n")
+    return render(request, 'time_display.html')
