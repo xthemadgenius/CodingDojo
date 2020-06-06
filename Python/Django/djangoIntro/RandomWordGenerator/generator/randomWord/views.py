d53b8f2ed93732db2327fae2e934d6b60201e667
Javier Calderon <javierjcjr@gmail.com>
2020-06-06 15:21:08 -0700

    more code

--- Python/Django/djangoIntro/RandomWordGenerator/generator/randomWord/views.py
+++ Python/Django/djangoIntro/RandomWordGenerator/generator/randomWord/views.py
@@ -2,12 +2,16 @@ from django.shortcuts import render, HttpResponse, redirect
 from django.utils.crypto import get_random_string
 
 def index(request):
-    return render(request, 'random_word.html')
+    word = get_random_string(length=14)
+    data = {
+        'random_word': word,
+    }
+    return render(request, 'random_word.html', data)
 
 def random(request):
     request.session['random_count'] += 1
-    return redirect('/')
+    return redirect('')
 
 def reset(request):
     request.session.clear()
-    return redirect('/')
+    return redirect('')
