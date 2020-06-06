e671f3af57265e5088d1704bab8ca2b0a5a250cf
Javier Calderon <javierjcjr@gmail.com>
2020-06-06 04:29:52 -0700

    more code

--- Python/PythonFundamentals/OOP/introToTDD.py
+++ Python/PythonFundamentals/OOP/introToTDD.py
@@ -1,3 +1,14 @@
+#1
+def reverseList(data_list):
+    length = len(data_list)
+    s = length
+    new_list = [None]*length
+    for item in data_list:
+        s = s - 1
+        new_list[s] = item
+    return new_list
+
+print(reverseList([1,2,3,4]))
 
 #2
 def isPalindrome(string):
