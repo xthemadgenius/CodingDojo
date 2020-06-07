f7f5ae3bf8f25ecc51c56889efcaaf7ff6e26c1f
Javier Calderon <javierjcjr@gmail.com>
2020-06-07 01:22:23 -0700

    more code

--- Python/Django/djangoIntro/NinjaGold/project/ninja_gold/views.py
+++ Python/Django/djangoIntro/NinjaGold/project/ninja_gold/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render, HttpResponse, redirect
 
 def index(request):
-    return HttpResponse("this is the equivalent of @app.route('/')!")
+    return render(request, 'index.html')
