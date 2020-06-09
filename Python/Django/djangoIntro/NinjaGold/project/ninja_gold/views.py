bb092daa70487ca29fce47ac3844ab07413e1b37
Javier Calderon <javierjcjr@gmail.com>
2020-06-08 18:12:52 -0700

    more code

--- Python/Django/djangoIntro/NinjaGold/project/ninja_gold/views.py
+++ Python/Django/djangoIntro/NinjaGold/project/ninja_gold/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render, HttpResponse, redirect
 
 def index(request):
     return render(request, 'index.html')
+
+def reset(request):
+    # if request.method == "POST":
+    #     request.session['total_gold'] = 0
+    #     request.session['activities'] = []
+    return redirect('/')
