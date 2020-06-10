87842077cef3dd1b8275686ec629ae19f1e726f1
Javier Calderon <javierjcjr@gmail.com>
2020-06-10 14:00:51 -0700

    more code

--- Python/Django/djangoORM/DojoAndNinjasShell/newProject/ninjaShell/models.py
+++ Python/Django/djangoORM/DojoAndNinjasShell/newProject/ninjaShell/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Dojo(models.Model):
+    name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Ninja(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    school = models.ForeignKey(Dojo, related_name="ninjas",on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
