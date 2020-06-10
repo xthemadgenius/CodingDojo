98c2a0c115f759c6afc2fea3c90ae920dcdaedc6
Javier Calderon <javierjcjr@gmail.com>
2020-06-09 17:22:03 -0700

    finished

--- Python/Django/djangoORM/userWTemplates/subTemp/userTemplate/views.py
+++ Python/Django/djangoORM/userWTemplates/subTemp/userTemplate/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render, HttpResponse
+
+
+
+
+
+
+from django.shortcuts import render, HttpResponse, redirect
+from .models import User
+
 def index(request):
-    return render(request, 'index.html')
+    all_users = User.objects.all()
+    context = {
+        'all_users': all_users
+    }
+    return render(request, 'index.html', context)
+
+def process(request):
+    print(request.POST)
+    User.objects.create(
+        first_name=request.POST["first_name"],
+        last_name=request.POST["last_name"],
+        email=request.POST['email'],
+        age=request.POST['age'],
+    )
+    return redirect('/')
