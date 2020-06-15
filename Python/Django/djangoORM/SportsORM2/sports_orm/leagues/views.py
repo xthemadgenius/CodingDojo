79ebaf1263b10320b58633dca61e945225e7eb19
Javier Calderon <javierjcjr@gmail.com>
2020-06-14 22:13:56 -0700

    finished Sports ORM 2

--- Python/Django/djangoORM/SportsORM2/sports_orm/leagues/views.py
+++ Python/Django/djangoORM/SportsORM2/sports_orm/leagues/views.py
@@ -12,8 +12,8 @@ def index(request):
 	context = {
 		"atlanticTeams": theTeam.filter(league__name__contains="Atlantic Soccer Conference"),
 		"penguinPlayers": thePlayer.filter(curr_team__team_name__contains="Penguin", curr_team__location__contains="Boston"),
-		"IntBaseballPlayers": thePlayer.filter(curr_team__league__name__contains="International Collegiate Baseball Conference"),
-		"lopezAmericanFooteball": thePlayer.filter(curr_team__league__name__contains="American Conference of Amateur Football", last_name__contains="Lopez"),
+		"intBaseballPlayers": thePlayer.filter(curr_team__league__name__contains="International Collegiate Baseball Conference"),
+		"lopezFooteball": thePlayer.filter(curr_team__league__name__contains="American Conference of Amateur Football", last_name__contains="Lopez"),
 		"footballPlayers": thePlayer.filter(curr_team__league__sport__contains="football"),
 		"findSophia": theTeam.filter(curr_players__first_name__contains="Sophia"),
 		"leagueWSophia": theLeague.filter(teams__curr_players__first_name__contains="Sophia"),
@@ -22,6 +22,9 @@ def index(request):
 		"tigerCatsWhere":thePlayer.filter(all_teams__location__contains="Manitoba", all_teams__team_name__contains="Tiger-Cats"),
 		"wasViking": thePlayer.filter(Q(all_teams__location__contains="Wichita", all_teams__team_name__contains="Vikings"), ~Q(curr_team__location__contains="Wichita", curr_team__team_name__contains="Vikings")),
 		"jacobWhere": theTeam.exclude(team_name="Colts", location="Oregon").filter(all_players__first_name__contains="Jacob", all_players__last_name__contains="Gray"),
+		"joshuaPlays": thePlayer.filter(first_name__contains="Joshua", all_teams__league__name="Atlantic Federation of Amateur Baseball Players"),
+		"tweleveOrMore": theTeam.annotate(count_players=Count("all_players")).filter(count_players__gte=12),
+		"teamArgByNum": thePlayer.annotate(count_teams=Count("all_teams")).order_by("-count_teams", "first_name"),
 	}
 	return render(request, "leagues/index.html", context)
 
