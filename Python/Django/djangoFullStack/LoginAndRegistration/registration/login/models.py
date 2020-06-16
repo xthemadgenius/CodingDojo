ef6ac321ccb56fea97389a44218122735b6901bb
Javier Calderon <javierjcjr@gmail.com>
2020-06-16 15:44:47 -0700

    better design for succes page

--- Python/Django/djangoFullStack/LoginAndRegistration/registration/login/models.py
+++ Python/Django/djangoFullStack/LoginAndRegistration/registration/login/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email  = models.EmailField(max_length=255)
+    birthday = models.DateField()
+    password = models.CharField(max_length=255)
+    confirm_password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
