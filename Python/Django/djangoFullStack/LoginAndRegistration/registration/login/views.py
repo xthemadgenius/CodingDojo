e685e716ef3a96609deefcbbee7e344c1bcb5851
Javier Calderon <javierjcjr@gmail.com>
2020-06-16 14:41:50 -0700

    styled lgin page

--- Python/Django/djangoFullStack/LoginAndRegistration/registration/login/views.py
+++ Python/Django/djangoFullStack/LoginAndRegistration/registration/login/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def index(request):
+    return render(request, 'login.html')
+
+def success(request):
+    return render(request, 'success.html')
