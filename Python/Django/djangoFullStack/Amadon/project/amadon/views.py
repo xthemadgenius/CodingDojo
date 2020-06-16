60b3c41864942f5887cac71e6145ab706842a9a4
Javier Calderon <javierjcjr@gmail.com>
2020-06-15 18:06:11 -0700

    Started Amadon

--- Python/Django/djangoFullStack/Amadon/project/amadon/views.py
+++ Python/Django/djangoFullStack/Amadon/project/amadon/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def index(request):
+    return render(request, "shop.html")
+
+def checkout(request):
+    return render(request, "checkout.html")
