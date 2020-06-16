f999ce9acac82533728a3d0654be1efc12192e33
Javier Calderon <javierjcjr@gmail.com>
2020-06-15 17:11:36 -0700

    finished TVShows Validate

--- Python/Django/djangoFullStack/Semi-RestfulTVShowsValidate/restful/tvShows/models.py
+++ Python/Django/djangoFullStack/Semi-RestfulTVShowsValidate/restful/tvShows/models.py
@@ -1,8 +1,37 @@
 from django.db import models
 from datetime import date, datetime
 from django.utils.dateparse import parse_date
+import re
 
-# Create your models here.
+class ShowManager(models.Manager):
+    EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+    def show_validator(self, postData):
+        errors = {}
+        if len(postData['title']) < 2:
+            errors['title'] = "Title should be at least 2 Characters"
+        if len(postData['tv_network']) < 3:
+            errors['tv_network'] = "Network should be at least 2 Characters"
+        if parse_date(postData['release_date']) > date.today():
+            errors['release_date'] = "Must enter a date from the Past"
+        if len(postData['desc']) > 0 and len(postData['desc']) < 10:
+            errors['desc'] = "Description should be at least 10 characters long, or be empty"
+        return errors
+    
+    def edit_validators(self, postData):
+        errors = {}
+        if len(postData['updated_title']) == 0:
+            errors["updated_title"] = "The Title of the show cannot be empty"
+        elif len(postData['updated_title']) < 2:
+            errors["updated_title"] = "The Title of the show has to be at least 2 characters long"
+        if len(postData["updated_tv_network"]) < 3:
+            errors["updated_tv_network"] = "The Network has to be at least 2 characters long"
+        if parse_date(postData["updated_release_date"]) > date.today():
+            errors["updated_release_date"] = "Must enter a date from the Past"
+        if len(postData["updated_desc"]) > 0 and len(postData['updated_desc']) < 10:
+            errors["updated_desc"] = "The Description has to be at least 10 characters long, or be empty"
+
+        return errors
+
 class Show(models.Model):
     title = models.CharField(max_length=255)
     tv_network = models.CharField(max_length=255)
@@ -10,3 +39,4 @@ class Show(models.Model):
     desc = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = ShowManager()
--- Python/Django/djangoFullStack/Semi-RestfulTVShowsValidate/restful/tvShows/views.py
+++ Python/Django/djangoFullStack/Semi-RestfulTVShowsValidate/restful/tvShows/views.py
@@ -12,13 +12,19 @@ def new(request):
     return render(request, "create.html")
 
 def create(request):
-    Show.objects.create(
-        title = request.POST['title'],
-        tv_network = request.POST['tv_network'],
-        release_date = request.POST['release_date'],
-        desc = request.POST['desc'],
-    )
-    return redirect("/shows")
+    errors = Show.objects.show_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request,value, extra_tags=key)
+        return redirect('/shows/new')
+    else:
+        Show.objects.create(
+            title = request.POST['title'],
+            tv_network = request.POST['tv_network'],
+            release_date = request.POST['release_date'],
+            desc = request.POST['desc'],
+        )
+        return redirect("/shows")
 
 
 def about(request, id):
@@ -34,13 +40,19 @@ def edit(request, id):
     return render(request, "edit.html", context)
 
 def update(request, id):
-    updated = Show.objects.get(id=id)
-    updated.title = request.POST['updated_title']
-    updated.tv_network = request.POST['updated_tv_network']
-    updated.release_date = request.POST['updated_release_date']
-    updated.desc = request.POST['updated_desc']
-    updated.save()
-    return redirect(f'/shows/{id}')
+    errors = Show.objects.edit_validators(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value, extra_tags=key)
+        return redirect(f'/shows/{id}/edit')
+    else:
+        updated = Show.objects.get(id=id)
+        updated.title = request.POST['updated_title']
+        updated.tv_network = request.POST['updated_tv_network']
+        updated.release_date = request.POST['updated_release_date']
+        updated.desc = request.POST['updated_desc']
+        updated.save()
+        return redirect(f'/shows/{id}')
 
 
 def destroy(request, id):
