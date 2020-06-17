549b8fc54f6c946b805a1c9b7004a23b2ed97413
Javier Calderon <javierjcjr@gmail.com>
2020-06-17 01:07:15 -0700

    finished project

--- Python/Django/djangoFullStack/LoginAndRegistration/registration/login/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index),
-    path('register', views.register),
-    path('success', views.success),
-    path('logout', views.logout),
-]
