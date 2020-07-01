fad6c328925af566bb4e7c2bc16dcc277e3fda76
xthemadgenius <javierjcjr@gmail.com>
2020-06-30 21:35:51 -0700

    finished Ninja

--- MERN/Javascript/OOP/ninja.js
+++ MERN/Javascript/OOP/ninja.js
@@ -1,5 +1,23 @@
 class Ninja{
-    constructor(name, health){
-        
-    }
+    constructor(name){
+        this.name = name;
+        this.health = 100;
+        this.speed = 3;
+        this.strength = 3;
+
+    Ninja.prototype.sayName = () => console.log(`Hello ${this.name}`);
+    // or
+    // Ninja.prototype.sayName = function(){
+    //     console.log("Hi my name is "+name+"!");
+    // }
+    Ninja.prototype.showStats = () => console.log(`Name: ${this.name}, Strength: ${this.strength}, Speed: ${this.speed}, Health: ${this.health}`);
+    // or 
+    // Ninja.prototype.showStats = function(){
+    //     console.log(`Name: ${this.name}, Strength: ${this.strength}, Speed: ${this.speed}, Health: ${this.health}`)
+    // }
+    Ninja.prototype.drinkSake = () => this.health = this.health + 10;
+    // or
+    // Ninja.prototype.drinkSake = function(){
+    //     this.health = this.health + 10;
+    // }
 }
