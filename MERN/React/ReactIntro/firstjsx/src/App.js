1815f1333955ba58622d81eff77eb3d06f08466b
xthemadgenius <javierjcjr@gmail.com>
2020-07-02 03:00:44 -0700

    finished another assignment

--- MERN/React/ReactIntro/firstjsx/src/App.js
+++ MERN/React/ReactIntro/firstjsx/src/App.js
@@ -7,17 +7,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello Dojo!</h1>
+        <h2>Things I need to do:</h2>
+        <ul>
+          <li>Learn React</li>
+          <li>Climb Mt. Everst</li>
+          <li>Run a marathon</li>
+          <li>Feed the dogs</li>
+        </ul>
       </header>
     </div>
   );
