fdf18ac8c001ca5f6f8b850f5b5b79c0acd05c39
xthemadgenius <javierjcjr@gmail.com>
2020-07-07 21:17:57 -0700

    refactored code to make it more simple and easier to read

--- MERN/Javascript/OOP/itsTimeToDuel.js
+++ MERN/Javascript/OOP/itsTimeToDuel.js
@@ -1,54 +1,102 @@
-class Card{
-    constructor(name, cost){
+class Player{
+    constructor(name){
         this.name = name;
-        this.cost = cost;
     }
 }
-class Unit extends Card{
+class Ninja extends Player{
     constructor(name, cost, power, res){
-        super(name, cost);
+        super(name);
+        this.cost = cost;
         this.power = power;
         this.res = res;
     }
+    redBeltNinja(){
+        var price = 3
+        if (this.cost >= this.cost - price){ 
+            this.cost -= price;
+            this.power += 3;
+            this.res += 4;
+        } else {
+            console.log("Can not afford card");
+        }
+    }
+    blackBeltNinja(){
+        var price = 4
+        if (this.cost >= this.cost - price){ 
+            this.cost -= price;
+            this.power += 5;
+            this.res += 4;
+        } else {
+            console.log("Can not afford card");
+        }
+    }
 }
-class DuelCards extends Unit{
+class DuelCards extends Ninja{
     constructor(name, cost, power, res){
         super(name, cost, power, res);
     }
-    redBeltNinja(){
-        this.cost = cost - 3;
-        this.power = 3;
-        this.resiliance = 4;
-    }
-    blackBeltNinja(){
-        this.cost = cost - 4;
-        this.power = 5;
-        this.resiliance = 4;
-    }
     hardAlgorithm(){
-        this.cost = cost - 2;
-        this.resiliance = resiliance + 3;
-        console.log(`Increase target's resiliance by 3`)
+        var price = 2;
+        if (this.cost >= this.cost - price){
+            this.cost -= price;
+            this.res += 3;
+            console.log(`${this.name} increaded their resiliance by 3`)
+        } else {
+            console.log("Can not afford card");
+        }
     }
-    unhandledPromiseRejection(){
-        this.cost = cost - 1;
-        this.resiliance = resiliance - 2;
-        console.log(`Reduce target's resiliance by 2`)
+    unhandledPromiseRejection(target){
+        var price = 1;
+        if (this.cost >= this.cost - price){
+            this.cost -= price;
+            target.res -= 2;
+            console.log(`${this.name} reduced ${target.name}'s resiliance by 2`)
+        } else {
+            console.log("Can not afford card");
+        }
     }
     pairProgramming(){
-        this.cost = cost - 3;
-        this.power = power + 2;
-        console.log(`Increse target's Power by 3`)
+        var price = 3;
+        if (this.cost >= this.cost - price){
+            this.cost -= price;
+            this.power += 2;
+            console.log(`${this.name} incresed their Power by 3`)
+        } else {
+            console.log("Can not afford card");
+        }
     }
     attack(target){
-        console.log(`${this.name} has attacked doing a damage of ${this.power} to ${target.name}`)
+        target.res -= this.power;
+        console.log(`${this.name} attacked ${target.name} doing a damage of ${this.power}`)
+        if( target.res > this.power){
+            console.log(`${target.name} survied the attack`);
+        } else {
+            console.log(`${target.name} lost the battle`);
+        }
     }
 }
-const player1 = new DuelCards("Jaxx");
-player1.redBeltNinja()
-player1.redBeltNinja().hardAlgorithm()
-const player2 = new DuelCards("Nacho");
-player2.blackBeltNinja();
-player2.blackBeltNinja().unhandledPromiseRejection();
-player1.redBeltNinja().pairProgramming();
-player1.attack(player2)
+//#1
+const player1 = new Player("Jaxx");
+const red = new Ninja(player1.name, 10, 10, 10)
+console.log(red);
+red.redBeltNinja()
+console.log(red);
+// #2
+const fighter1 = new DuelCards(red.name, red.cost,red.power, red.res);
+fighter1.hardAlgorithm(fighter1)
+console.log(fighter1);
+// #3
+const player2 = new Player("Nacho");
+const black = new Ninja(player2.name,10, 10, 10);
+console.log(black);
+black.blackBeltNinja();
+console.log(black);
+// #4
+const fighter2 = new DuelCards(black.name, black.cost,black.power, black.res);
+fighter2.unhandledPromiseRejection(fighter1);
+console.log(fighter1);
+// #5
+fighter1.pairProgramming();
+console.log(fighter1);
+// #6
+fighter1.attack(fighter2);
--- MERN/Javascript/OOP/superNinja.js
+++ MERN/Javascript/OOP/superNinja.js
@@ -1,102 +1,37 @@
-class Player{
+class Ninja{
     constructor(name){
         this.name = name;
+        this.health = 100;
+        this.strength = 3;
+        this.speed = 3;
+    }
+    sayName(){
+        console.log(`My name is ${this.name}`);
+    }
+    showStats(){
+        console.log(`Name: ${this.name}, Strength: ${this.strength}, Speed: ${this.speed}, Health: ${this.health}`);
+    }
+    drinkSake(){
+        this.health += 10;
     }
 }
-class Ninja extends Player{
-    constructor(name, cost, power, res){
-        super(name);
-        this.cost = cost;
-        this.power = power;
-        this.res = res;
+
+class Sensei extends Ninja{
+    constructor(name){
+        super(name, 200, 10, 10);
+        this.wisdom = 10;
     }
-    redBeltNinja(){
-        var price = 3
-        if (this.cost >= this.cost - price){ 
-            this.cost -= price;
-            this.power += 3;
-            this.res += 4;
-        } else {
-            console.log("Can not afford card");
-        }
-    }
-    blackBeltNinja(){
-        var price = 4
-        if (this.cost >= this.cost - price){ 
-            this.cost -= price;
-            this.power += 5;
-            this.res += 4;
-        } else {
-            console.log("Can not afford card");
-        }
+    speakWisdom(){
+        super.drinkSake();
+        console.log("Bugs only exist in the mind, errors exist in people");
     }
 }
-class DuelCards extends Ninja{
-    constructor(name, cost, power, res){
-        super(name, cost, power, res);
-    }
-    hardAlgorithm(){
-        var price = 2;
-        if (this.cost >= this.cost - price){
-            this.cost -= price;
-            this.res += 3;
-            console.log(`${this.name} increaded their resiliance by 3`)
-        } else {
-            console.log("Can not afford card");
-        }
-    }
-    unhandledPromiseRejection(target){
-        var price = 1;
-        if (this.cost >= this.cost - price){
-            this.cost -= price;
-            target.res -= 2;
-            console.log(`${this.name} reduced ${target.name}'s resiliance by 2`)
-        } else {
-            console.log("Can not afford card");
-        }
-    }
-    pairProgramming(){
-        var price = 3;
-        if (this.cost >= this.cost - price){
-            this.cost -= price;
-            this.power += 2;
-            console.log(`${this.name} incresed their Power by 3`)
-        } else {
-            console.log("Can not afford card");
-        }
-    }
-    attack(target){
-        target.res -= this.power;
-        console.log(`${this.name} attacked ${target.name} doing a damage of ${this.power}`)
-        if( target.res > this.power){
-            console.log(`${target.name} survied the attack`);
-        } else {
-            console.log(`${target.name} lost the battle`);
-        }
-    }
-}
-//#1
-const player1 = new Player("Jaxx");
-const red = new Ninja(player1.name, 10, 10, 10)
-console.log(red);
-red.redBeltNinja()
-console.log(red);
-// #2
-const fighter1 = new DuelCards(red.name, red.cost,red.power, red.res);
-fighter1.hardAlgorithm(fighter1)
-console.log(fighter1);
-// #3
-const player2 = new Player("Nacho");
-const black = new Ninja(player2.name,10, 10, 10);
-console.log(black);
-black.blackBeltNinja();
-console.log(black);
-// #4
-const fighter2 = new DuelCards(black.name, black.cost,black.power, black.res);
-fighter2.unhandledPromiseRejection(fighter1);
-console.log(fighter1);
-// #5
-fighter1.pairProgramming();
-console.log(fighter1);
-// #6
-fighter1.attack(fighter2);
+
+let ninja = new Ninja("Donatello", 100);
+ninja.showStats();
+ninja.drinkSake();
+ninja.showStats();
+let sensei = new Sensei("Master Splinter");
+sensei.showStats();
+sensei.speakWisdom();
+sensei.showStats();
