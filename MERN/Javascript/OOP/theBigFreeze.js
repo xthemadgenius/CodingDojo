44a524d0f2e7e08e58f27686b3000da03b99a213
xthemadgenius <javierjcjr@gmail.com>
2020-07-07 22:26:39 -0700

    done testing

--- MERN/Javascript/OOP/theBigFreeze.js
+++ MERN/Javascript/OOP/theBigFreeze.js
@@ -27,3 +27,24 @@ console.log(numbers);
 const sortedGroceries = [...groceryList].sort( (a, b) => a.item > b.item );
 console.log(sortedGroceries);
 
+// part 3
+const groceries = ["pearl onions", "cremini mushrooms", "thyme"];
+const groceryList = groceries.map( item => `<li>${item}</li>` );
+console.log(groceryList);
+
+const values = [1, 2, 3, 4, 5];
+const cubes = values.map( val => val**3 );
+console.log(cubes)
+
+// part 4
+const values = [1, 2, 3, 4, 5];
+const evens = values.filter( val => val % 2 === 0 );
+console.log(evens);
+
+const groceries = ["pearl onions", "cremini mushrooms", "thyme"];
+const oFoods = groceries.filter( item => item.includes("o") );
+console.log(oFoods);
+
+const values = [1, 2, 3, 4, 5];
+const oddCubes = values.filter( val => val % 2 !==0 ).map( val => val**3 );
+console.log(oddCubes);
