572aeef8a4b4e92d589eb549d63bccb29effc07c
xthemadgenius <javierjcjr@gmail.com>
2020-07-16 00:51:23 -0700

    new code

--- MERN/Express/RunningTestPost/HelloExpress/HelloExpress/server.js
+++ MERN/Express/RunningTestPost/HelloExpress/HelloExpress/server.js
@@ -1,28 +1,79 @@
-// const express = require("express");
-// const app = express();
-
-// // req is short for request
-// // res is short for response
-// app.get("/api", (req, res) => {
-//   res.send("Our express api server is now sending this over to the browser");
-// });
-
-// const server = app.listen(8000, () =>
-//   console.log(`Server is locked and loaded on port ${server.address().port}!`)
-// );
-
-// const port = 8000;
-
 const express = require("express");
 const app = express();
 const port = 8000;
     
+
+// make sure these lines are above any app.get or app.post code blocks
+app.use( express.json() );
+app.use( express.urlencoded({ extended: true }) );
+
 // req is shorthand for request
 // res is shorthand for response
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello World" });
+    res.json({ message: "Hello World Boel fix me" });
+});
+
+const users = [
+  { firstName: "Reimu",  lastName: "Hakurei"    },
+  { firstName: "Marisa", lastName: "Kirisame"   },
+  { firstName: "Sanae",  lastName: "Kochiya"    },
+  { firstName: "Sakuya", lastName: "Izayoi"     },
+  { firstName: "Momiji", lastName: "Inubashiri" }
+];
+
+app.post("/api/users", (req, res) => {
+  // req.body will contain the form data from Postman or from React
+  console.log(req.body);
+  // we can push it into the users array for now...
+  // later on this will be inserted into a database
+  users.push(req.body);
+  // we always need to respond with something
+  res.json( { status: "ok" } );
 });
 
 
+app.get("/api/users", (req, res) => {
+  res.json( users );
+});
+
+// get data
+// if we want to get a user with a specific id, we can make the id a part of the url
+// be sure to preface the id variable with a `:` colon
+app.get("/api/users/:id", (req, res) => {
+  // we can get this `id` variable from req.params
+  console.log(req.params.id);
+  // assuming this id is the index of the users array we could return one user this way
+  res.json( users[req.params.id] );
+});
+
+// updates data
+app.put("/api/users/:id", (req, res) => {
+  // we can get this `id` variable from req.params
+  const id = req.params.id;
+  // assuming this id is the index of the users array we can replace the user like so
+  users[id] = req.body;
+  // we always need to respond with something
+  res.json( { status: "ok" } );
+});
+
+// delete data
+app.delete("/api/users/:id", (req, res) => {
+  // we can get this `id` variable from req.params
+  const id = req.params.id;
+  // assuming this id is the index of the users array we can remove the user like so
+  users.splice(id, 1);
+  // we always need to respond with something
+  res.json( { status: "ok" } );
+});
+
+// updates data
+app.put("/api/users/:id", (req, res) => {
+  // we can get this `id` variable from req.params
+  const id = req.params.id;
+  // assuming this id is the index of the users array we can replace the user like so
+  users[id] = req.body;
+  // we always need to respond with something
+  res.json( { status: "ok" } );
+});
 // this needs to below the other code blocks
 app.listen( port, () => console.log(`Listening on port: ${port}`) );
