07748e7d3d4eccfdfa6d1427ec5dc5c1c818cf00
xthemadgenius <javierjcjr@gmail.com>
2020-07-16 02:34:34 -0700

    finished Faker API

--- MERN/Express/FakerAPI/server.js
+++ MERN/Express/FakerAPI/server.js
@@ -1,23 +1,66 @@
 const express = require("express");
 const faker = require("faker");
+const port = 8000;
 const app = express();
 
-class Product {
-    constructor(){
-        this.name = faker.commerce.productName();
-        this.price = faker.commerce.price();
-        this.department = faker.commerce.department();
-    }
-}
-console.log(new Product());
 
 class User {
     constructor(){
-        this.firstName = faker.commerce.productName();
-        this.lastName = faker.commerce.price();
-        this.phoneNumber = faker.commerce.department();
-        this.email = faker.commerce.price();
-        this.password = faker.commerce.department();
+        this._id = faker.random.uuid();
+        this.firstName = faker.name.firstName();
+        this.lastName = faker.name.lastName();
+        this.phoneNumber = faker.phone.phoneNumber();
+        this.email = faker.internet.email();
+        this.password = faker.internet.password();
     }
 }
 console.log(new User());
+
+class Company {
+    constructor(){
+        this._id = faker.random.uuid();
+        this.name = faker.company.companyName();
+        this.street = faker.address.streetName();
+        this.city = faker.address.city();
+        this.state = faker.address.state();
+        this.zipCode = faker.address.zipCode();
+        this.country = faker.address.country();
+    }
+}
+console.log(new Company());
+
+app.get('/api/users/new', (req,res) =>{
+    var user = new User();
+    res.json({
+        results: user
+    });
+    
+})
+
+app.get('/api/companies/new', (req,res) =>{
+    var company = new Company();
+    res.json({
+        results: company
+    });
+    
+})
+
+app.get('/api/user/company', (req, res) =>{
+    var user = new User();
+    var company = new Company();
+    res.json({
+        results: {
+            user: user,
+            company: company,
+        }
+    })
+})
+
+const server = app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+// tested get requests
+
+// http://localhost:8000/api/users/new
+// http://localhost:8000/api/companies/new
+// http://localhost:8000/api/user/company
