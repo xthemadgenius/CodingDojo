c1c13e61d6f1701e9a848780935c80f88c430740
xthemadgenius <javierjcjr@gmail.com>
2020-07-18 19:43:37 -0700

    created a folder for part 1

---
+++ MERN/Full-Stack-Mern/ProductManager/part3/client/src/views/Main.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ProductForm from '../components/ProductForm';
+
+export default () =>{
+    return (
+        <div>
+            <ProductForm/>
+        </div>
+    )
+}
---
+++ MERN/Full-Stack-Mern/ProductManager/part3/server/controllers/product.controller.js
@@ -0,0 +1,24 @@
+const { Product } = require('../models/product.model')
+
+module.exports.index = (req, res) => {
+    res.json({
+        message: "Hello World"
+    });
+}
+
+module.exports.createProduct = (req, res) => {
+    const {title, price, description } = req.body;
+    Product.create({
+        title,
+        price,
+        description
+    })
+    .then(product => res.json(product))
+    .catch(err => res.json(err));
+}
+
+module.exports.findProd = (req, res) => {
+    Product.find()
+    .then(prod => res.json(res.json(prod)))
+    .catch(err => res.json(res.json(err)));
+};
