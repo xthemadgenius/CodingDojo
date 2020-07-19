a84d36dc2137893155c0bc630fecbb8f00408bb6
xthemadgenius <javierjcjr@gmail.com>
2020-07-18 18:17:01 -0700

    working on connecting front to backend

--- MERN/Full-Stack-Mern/ProductManager/part1/server.js
+++ MERN/Full-Stack-Mern/ProductManager/part1/server.js
@@ -1,6 +1,9 @@
 express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 8000;
+
+app.use(cors());
 require('./server/routes/product.routes')(app);
 
 app.listen(port, () => {
