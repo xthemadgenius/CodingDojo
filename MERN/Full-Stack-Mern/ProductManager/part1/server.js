ceaca6f7cdbb70557224ba292087f7a295ec4e8f
xthemadgenius <javierjcjr@gmail.com>
2020-07-18 18:01:06 -0700

    got the API working

--- MERN/Full-Stack-Mern/ProductManager/part1/server.js
+++ MERN/Full-Stack-Mern/ProductManager/part1/server.js
@@ -1,5 +1,8 @@
 express = require('express');
 const app = express();
 const port = 8000;
+require('./server/routes/product.routes')(app);
 
-app.listen(port, () => console.log(`Listening on port: ${port}`))
+app.listen(port, () => {
+    console.log(`Listening on port: ${port}`)
+})
