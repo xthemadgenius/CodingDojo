ae56d53c29d5d39d7b94b78500ee5c33e76b0133
xthemadgenius <javierjcjr@gmail.com>
2020-07-24 12:00:14 -0700

    finished?

--- MERN/MernBeltTest/client/src/views/AddPet.js
+++ MERN/MernBeltTest/client/src/views/AddPet.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import PetForm from '../components/PetForm';
-import { Link } from '@reach/router';
+import { Link, navigate } from '@reach/router';
+import Axios from 'axios';
 
 const AddPet = (props) => {
 
+    const onSubmitHandler = (e, data) => {
+        e.preventDefault();
+        Axios.post(`http://localhost:8000/api/belts/`, data)
+        .then(res => {
+            navigate('/')
+        })
+        .catch(err => console.log(err));
+    }
     return (
         <div>
             <h1>Know a Pet needing a Home</h1>
             <Link to="/">Back to home</Link>
-            <PetForm/>
+            <PetForm
+            onSubmitHandler={onSubmitHandler}
+            initialName=""
+            initialPetType=""
+            initialDescription=""
+            initialSkill1=""
+            initialSkill2=""
+            initialSkill3=""
+            />
         </div>
     )
 }
--- MERN/MernBeltTest/client/src/views/EditPet.js
+++ MERN/MernBeltTest/client/src/views/EditPet.js
@@ -1,14 +1,48 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import PetForm from '../components/PetForm';
-import { Link } from '@reach/router';
-
+import { Link, navigate } from '@reach/router';
+import axios from 'axios';
 const EditPet = (props) => {
+
+    const {id} = props;
+
+    const [pet, setPet] = useState({});
+    const [loaded, setLoaded] = useState(false);
+
+    useEffect(() => {
+        axios.get(`http://localhost:8000/api/belts/${id}`)
+        .then((res) => {
+            setPet(res.data);
+            setLoaded(true);
+        })
+        .catch(err => console.log(err));
+    })
+
+    const onSubmitHandler = (e, data) => {
+        e.preventDefault();
+        axios.put(`http://localhost:8000/api/belts/${id}`, data)
+        .then(res => {
+            navigate('/')
+        })
+        .catch(err => console.log(err));
+    }
 
     return (
         <div>
-            <h1>Edit PETNAME</h1>
+            <h1>Edit {pet.name}</h1>
             <Link to="/">Back to home</Link>
-            <PetForm/>
+            {loaded &&
+                <PetForm
+                onSubmitHandler={onSubmitHandler}
+                initialName={pet.name}
+                initialPetType={pet.petType}
+                initialDescription={pet.description}
+                initialSkill1={pet.skill1}
+                initialSkill2={pet.skill2}
+                initialSkill3={pet.skill3}
+                />
+            }
+            
         </div>
     )
 }
