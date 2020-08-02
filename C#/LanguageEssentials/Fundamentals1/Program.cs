c1284e7f2f79537853a268a8a6606e480332d9b6
xthemadgenius <javierjcjr@gmail.com>
2020-08-02 15:46:03 -0700

    fixed the code

--- C#/LanguageEssentials/Fundamentals1/Program.cs
+++ C#/LanguageEssentials/Fundamentals1/Program.cs
@@ -6,23 +6,23 @@ namespace Fundamentals1
     {
         static void Main(string[] args)
         {
-            // int large = 255;
-            // for(int i = 1; i <= large; i++)
-            // {
-            //     Console.WriteLine(i);
-            // }
-            // int hundo = 100;
-            // for(int i = 1; i <= hundo; i++)
-            // {
-            //     if(i % 3 == 0)
-            //     {
-            //         Console.WriteLine(i);
-            //     }
-            //     else if(i % 5 == 0)
-            //     {
-            //         Console.WriteLine(i);
-            //     }
-            // }
+            int large = 255;
+            for(int i = 1; i <= large; i++)
+            {
+                Console.WriteLine(i);
+            }
+            int hundo = 100;
+            for(int i = 1; i <= hundo; i++)
+            {
+                if(i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                else if(i % 5 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
             int gudo = 100;
             for(int i = 1; i <= gudo; i++)
             {
