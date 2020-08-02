a22fc8708cd91e95fdb87d295dcc8fe42407a3b8
xthemadgenius <javierjcjr@gmail.com>
2020-08-02 15:43:21 -0700

    finished Fundamentals 1

---
+++ C#/LanguageEssentials/Fundamentals1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Fundamentals1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int large = 255;
+            // for(int i = 1; i <= large; i++)
+            // {
+            //     Console.WriteLine(i);
+            // }
+            // int hundo = 100;
+            // for(int i = 1; i <= hundo; i++)
+            // {
+            //     if(i % 3 == 0)
+            //     {
+            //         Console.WriteLine(i);
+            //     }
+            //     else if(i % 5 == 0)
+            //     {
+            //         Console.WriteLine(i);
+            //     }
+            // }
+            int gudo = 100;
+            for(int i = 1; i <= gudo; i++)
+            {
+                if(i % 5 == 0 && i % 3 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if(i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if(i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+            }
+        }
+    }
+}
--- C#/README.md
+++ C#/README.md
@@ -27,4 +27,16 @@ To Run C# Project:
 
 dotnet run
 
+int	    Int32	    An integer value between -2,147,483,648 and 2,147,483,647	32-bits	40,000
 
+long	Int64	    An integer between -9,223,372,036,854,775,808 and 9,223,372,036,854,775,807	64-bits	3,000,000,000
+
+short	Int16	    An integer between -32,768 and 32,767	16-bits	342
+
+float	Single	    A floating point number between -3.402823e38 and 3.402823e38	32-bits	3.14159f
+
+double	Double	    A floating point number between -1.79769313486232e308 and 1.79769313486232e308. Allows more precision than a float.	64-bits	3.1415926535897932
+
+char	Char	    One unicode character	16-bits	'g'
+
+bool	Boolean	    A true or false value	8-bits	true
