e3072d4ae15935dde440e5f90c690251ecf7a5fc
xthemadgenius <javierjcjr@gmail.com>
2020-08-03 10:34:40 -0700

    finished part 1 of 3

--- C#/LanguageEssentials/Puzzles/Program.cs
+++ C#/LanguageEssentials/Puzzles/Program.cs
@@ -13,11 +13,28 @@ namespace Puzzles
             {
                 randArr[i] = rand.Next(5,25);
             }
+
+            int min = int.MaxValue;
+            int max = int.MinValue;
+            int sum = 0;
+            for (int i = 0; i < randArr.Length; i++)
+            {
+                if(randArr[i] < min)
+                {
+                    min = randArr[i];
+                }
+                if(randArr[i] > max)
+                {
+                    max = randArr[i];
+                }
+                sum += randArr[i];
+            }
+            Console.WriteLine($"Min: {min} Max: {max} Sum: {sum}");
             return randArr;
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            RandomArray();
         }
     }
 }
