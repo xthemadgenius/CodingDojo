b846e1744dd98d12b1655b333587c3195b6a17f6
xthemadgenius <javierjcjr@gmail.com>
2020-08-03 02:05:58 -0700

    finished first 3 functions

--- C#/LanguageEssentials/Basic13/Program.cs
+++ C#/LanguageEssentials/Basic13/Program.cs
@@ -4,9 +4,39 @@ namespace Basic13
 {
     class Program
     {
+        public static void printNumbers()
+        {
+            for(int i = 0; i <= 255; i ++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        public static void printOdd()
+        {
+            for(int i = 0; i <= 255; i ++)
+            {
+                if(i % 2 != 0){
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        public static void PrintSum()
+        {
+            int sum = 0;
+            for(int i = 0; i <= 255; i ++)
+            {
+                sum += i;
+                Console.WriteLine($"New Number: {i} Sum: {sum}");
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // printNumbers();
+            // printOdd();
+            PrintSum();
         }
     }
 }
