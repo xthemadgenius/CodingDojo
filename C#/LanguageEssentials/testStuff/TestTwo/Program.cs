a010852b4004cd01b30a5c7c35a15fe462d0bd01
xthemadgenius <javierjcjr@gmail.com>
2020-08-03 01:27:21 -0700

    running tests

---
+++ C#/LanguageEssentials/testStuff/TestTwo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TestTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Converting an integer to a string
+        int num = 7;
+        string numAsString = num.ToString(); //Evaluates as "7"
+        Console.WriteLine(numAsString);
+        }
+    }
+}
