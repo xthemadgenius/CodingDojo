a99727ac43eb449526d64f92ff30a812c57eafb6
xthemadgenius <javierjcjr@gmail.com>
2020-08-03 01:46:03 -0700

    started Basic 13

---
+++ C#/LanguageEssentials/Basic13/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Basic13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
