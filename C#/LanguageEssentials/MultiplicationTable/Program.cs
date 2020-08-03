a848712361288bc9bc00d4712e4d1592e55b6a08
xthemadgenius <javierjcjr@gmail.com>
2020-08-03 01:12:08 -0700

    Finished multiplication table

--- C#/LanguageEssentials/MultiplicationTable/Program.cs
+++ C#/LanguageEssentials/MultiplicationTable/Program.cs
@@ -6,7 +6,29 @@ namespace MultiplicationTable
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[,] multiplicationTable = new int[11,11];
+            string output = "";
+            for(int i = 0; i < 10; i++)
+            {
+                for(int j = 0; j < 10; j++)
+                {
+                    multiplicationTable[i + 1, j + 1] = ((i + 1) * (j + 1));
+
+                    if(j == 0)
+                    {
+                        output += $"[{((i + 1) * (j + 1))}, ";
+                    }
+                    else if(j == 9)
+                    {
+                        output += $"{((i + 1) * (j + 1))}]\n";
+                    }
+                    else
+                    {
+                        output += $"{((i + 1) * (j + 1))}, ";
+                    }
+                }
+            }
+            Console.WriteLine(output);
         }
     }
 }
