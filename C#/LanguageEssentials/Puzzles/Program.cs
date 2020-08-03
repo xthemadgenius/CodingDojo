39eed78e135a8a152277f50e549ff882bef3c43f
xthemadgenius <javierjcjr@gmail.com>
2020-08-03 10:25:31 -0700

    moere code

--- C#/LanguageEssentials/Puzzles/Program.cs
+++ C#/LanguageEssentials/Puzzles/Program.cs
@@ -1,9 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Puzzles
 {
     class Program
     {
+        public static int[] RandomArray()
+        {
+            int[] randArr = new int[10];
+            Random rand = new Random();
+            for(int i = 0; i < randArr.Length; i++)
+            {
+                randArr[i] = rand.Next(5,25);
+            }
+            return randArr;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
