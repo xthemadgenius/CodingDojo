31df21b91eb747cac346876927ddffe797f05b78
xthemadgenius <javierjcjr@gmail.com>
2020-08-04 16:01:49 -0700

    did Ninja Part

---
+++ C#/OOP/WizardNinjaSamurai/Ninja.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace WizardNinjaSamurai
+{
+    class Ninja : Human
+    {
+        public Ninja(string name) : base(name)
+        {
+            Dexterity = 175;
+        }
+
+        public override int Attack(Human target)
+        {
+            Random hit = new Random();
+            int dmg = Dexterity * 5;
+            if (hit.Next(5) == 1)
+            {
+                dmg += 10;
+            }
+            target.health -= dmg;
+            Console.WriteLine($"{Name} attacked {target.Name} for {dmg} damage!");
+            return target.health;
+        }
+
+        public void Steal(Human target)
+        {
+            target.health -= 5;
+            health += 5;
+            Console.WriteLine($"{Name} stole {target.Name}'s hit points for 5 damage!");
+        }
+    }
+}
--- C#/OOP/WizardNinjaSamurai/Wizard.cs
+++ C#/OOP/WizardNinjaSamurai/Wizard.cs
@@ -10,19 +10,19 @@ namespace WizardNinjaSamurai
             health = 50;
         }
 
-        public int Heal(Human target)
-        {
-            int heal = 10 * Intelligence;
-            target.health += heal;
-            Console.WriteLine($"{Name} healed {target.Name} for {heal} hit points!");
-            return target.health;
-        }
-
         public override int Attack (Human target)
         {
             int dmg = Intelligence * 5;
             target.health -= dmg;
             Console.WriteLine($"{Name} attacked {target.Name} for {dmg} hit points!");
+            return target.health;
+        }
+
+        public int Heal(Human target)
+        {
+            int heal = 10 * Intelligence;
+            target.health += heal;
+            Console.WriteLine($"{Name} healed {target.Name} for {heal} hit points!");
             return target.health;
         }
     }
