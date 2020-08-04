25a3965283ed6cd6153001f96d936fbaa1cbc7d6
xthemadgenius <javierjcjr@gmail.com>
2020-08-04 05:55:25 -0700

    finished Deck Of cards

--- C#/OOP/DeckOfCards/Program.cs
+++ C#/OOP/DeckOfCards/Program.cs
@@ -11,14 +11,17 @@ namespace DeckOfCards
             deck.Shuffle();
             Player player = new Player("Debo");
             Console.WriteLine(player.Name);
+
             for(int i = 0; i < 6; i ++)
             {
                 player.Draw(deck);
             }
+
             foreach(var card in player.Hand)
             {
                 card.SayCard();
             }
+            
             player.Discard(3);
             Console.WriteLine("Discarded a card");
             foreach(var card in player.Hand)
