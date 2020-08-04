e486a38dadda1ac88382f7c14da55ca108874641
xthemadgenius <javierjcjr@gmail.com>
2020-08-04 03:48:41 -0700

    did human page

---
+++ C#/OOP/Human/Human.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace Human
+{
+    public class Human
+    {
+        public string Name;
+        public int Strength;
+        public int Intelligence;
+        public int Dexterity;
+        public int health;
+
+        public int Health
+        {
+            get {return health;}
+        }
+
+        public Human(string name)
+        {
+            Name = name;
+            Strength = 3;
+            Intelligence = 3;
+            Dexterity = 3;
+            health = 100;
+        }
+
+        public Human(string name, int strength, int intelligence, int dexterity, int health)
+        {
+            Name = name;
+            Strength = strength;
+            Intelligence = intelligence;
+            Dexterity = dexterity;
+            this.health = health;
+        }
+
+        public int Attack(Human target)
+        {
+            target.health = (5 * Strength);
+            return target.health;
+        }
+    }
+}
