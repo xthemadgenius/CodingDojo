156d78971e44d204f0b164de7ea0368ace27dadd
xthemadgenius <javierjcjr@gmail.com>
2020-08-05 11:32:05 -0700

    preset files

---
+++ C#/OOP/TerminalRPGEncounter/Human.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace TerminalRPGEncounter
+{
+    class Human
+    {
+        public string Name;
+        public int Strength;
+        public int Intelligence;
+        public int Dexterity;
+        public int health;
+
+        public int Health
+        {
+            get
+            {
+                return health;
+            }
+            set {}
+        }
+    }
+}
