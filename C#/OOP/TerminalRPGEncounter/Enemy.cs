c1dfe34a00cbaf387944cd46ddc9ac156e07ec3c
xthemadgenius <javierjcjr@gmail.com>
2020-08-05 11:42:25 -0700

    set up Human class starting Enemy class

--- C#/OOP/TerminalRPGEncounter/Enemy.cs
+++ C#/OOP/TerminalRPGEncounter/Enemy.cs
@@ -2,5 +2,14 @@ using System;
 
 namespace TerminalRPGEncounter
 {
-    
+    class Enemy
+    {
+        public string Name;
+        public int health;
+        public Enemy(string name, int healthPoints)
+        {
+            Name = name;
+            health = healthPoints;
+        }
+    }
 }
--- C#/OOP/TerminalRPGEncounter/Human.cs
+++ C#/OOP/TerminalRPGEncounter/Human.cs
@@ -18,5 +18,38 @@ namespace TerminalRPGEncounter
             }
             set {}
         }
+
+        public Human(string name)
+        {
+            Name = name;
+            Strength = 3;
+            Intelligence = 3;
+            Dexterity = 3;
+            health = 100;
+        }
+
+        public Human(string name, int strength, int intelligence, int dexterity, int healthPoints)
+        {
+            Name = name;
+            Strength = strength;
+            Intelligence = intelligence;
+            Dexterity = dexterity;
+            health = healthPoints;
+        }
+
+        public virtual int Attack(Enemy target)
+        {
+            int dmg = Strength * 3;
+            target.health -= dmg;
+            Console.WriteLine($"{Name} atthacked {target.Name} making {dmg} damage");
+            return dmg;
+        }
+
+        public virtual int Attack(Enemy target, int dmg)
+        {
+            target.health -= dmg;
+            Console.WriteLine($"{Name} atthacked {target.Name} making {dmg} damage");
+            return dmg;
+        }
     }
 }
