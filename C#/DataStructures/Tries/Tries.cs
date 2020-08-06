940cd80bd4706c581bc7f974360d3443e521bd7e
xthemadgenius <javierjcjr@gmail.com>
2020-08-06 02:19:09 -0700

    need help to solve thes 3 assignments in Data Strutures, glad that these are optional, have to move forward o next lesson plan, will come back later

---
+++ C#/DataStructures/Tries/Tries.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Tries
+{
+    class Tries
+    {
+        public void Add(string word)
+        {
+
+        }
+
+        public bool Contains(string word)
+        {
+            return true;
+        }
+
+        public string[] Autocomplete(string prefix)
+        {
+            string[] word = new string[{},{}];
+            return word;
+        }
+
+    }
+}
