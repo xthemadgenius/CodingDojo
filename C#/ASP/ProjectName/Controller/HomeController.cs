75726409226e61f35b867c216189775aa6c389c9
xthemadgenius <javierjcjr@gmail.com>
2020-08-06 22:17:05 -0700

    Started Portfolio 1

--- C#/ASP/ProjectName/Controller/HomeController.cs
+++ C#/ASP/ProjectName/Controller/HomeController.cs
@@ -10,5 +10,19 @@ namespace ProjectName.Controllers     //be sure to use your own project's namesp
         {
             return "Hello World from HomeController!";
         }
+
+        [HttpGet("about")]
+// GET requests to "localhost:5000/about" go here
+        public string About()
+        {
+            return "Mexico is for Mexicans";
+        }
+
+        [HttpGet]
+        [Route("pizza/{topping}")]
+        public string PizzaParty(string topping)
+        {
+            return $"Who's ready for a {topping} Pizza!";
+        }
     }
 }
