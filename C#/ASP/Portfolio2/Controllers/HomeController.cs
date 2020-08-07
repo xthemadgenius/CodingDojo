991a6f20aad69c6f48ce3de33bc0324c96dc0089
xthemadgenius <javierjcjr@gmail.com>
2020-08-07 00:31:12 -0700

    set up files to operate Portfolio 2

---
+++ C#/ASP/Portfolio2/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+namespace Portfolio2.Controllers     //be sure to use your own project's namespace!
+{
+    public class HomeController : Controller   //remember inheritance??copy
+    {
+        //for each route this controller is to handle:
+        [HttpGet]       //type of request
+        [Route("")]     //associated route string (exclude the leading /)
+        public string Index()
+        {
+            return "Hello World from HomeController!";
+        }
+
+        [HttpGet]       //type of request
+        [Route("about")]     //associated route string (exclude the leading /)
+        public ViewResult About()
+        {
+            return View("Index");
+        }
+    }
+}
