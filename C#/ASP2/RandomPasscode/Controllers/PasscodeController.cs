0c9782fc8b97d63d797d743dea3d22789d8963ee
xthemadgenius <javierjcjr@gmail.com>
2020-08-09 14:07:46 -0700

    Finished Random Passcode

--- C#/ASP2/RandomPasscode/Controllers/PasscodeController.cs
+++ C#/ASP2/RandomPasscode/Controllers/PasscodeController.cs
@@ -1,18 +1,44 @@
 using System;
-using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
-// using RandomPasscode.Models;
 
 namespace RandomPasscode.Controllers     //be sure to use your own project's namespace!
 {
     public class PasscodeController : Controller
     {
-        [HttpGet]    
-        [Route("")] 
-        public string Index()
+        [HttpGet("")] 
+        public ViewResult Index()
         {
-            return "Hello World from HomeController!";
+            if(HttpContext.Session.GetInt32("Count") == null)
+            {
+                HttpContext.Session.SetInt32("Count", 0);
+            }
+            ViewBag.Count = HttpContext.Session.GetInt32("Count");
+            ViewBag.String = HttpContext.Session.GetString("RandCode");
+            return View();
+        }
+
+        [HttpGet("generate")]
+        public IActionResult Generate()
+        {
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            char[] randomString = new char[14];
+            Random rand = new Random();
+            for (int i = 0; i < 14; i++)
+            {
+                randomString[i] = chars[rand.Next(chars.Length)];
+            }
+            string newString = new String(randomString);
+            HttpContext.Session.SetString("RandCode", newString);
+            HttpContext.Session.SetInt32("Count", (int)HttpContext.Session.GetInt32("Count") + 1);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet("reset")]
+        public IActionResult Reset()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index");
         }
     }
 }
