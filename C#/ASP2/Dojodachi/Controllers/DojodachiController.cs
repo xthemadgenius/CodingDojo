625b0cd72a20bfd485fc2eddc5238d0b089fb212
xthemadgenius <javierjcjr@gmail.com>
2020-08-09 22:26:12 -0700

    more code

--- C#/ASP2/Dojodachi/Controllers/DojodachiController.cs
+++ C#/ASP2/Dojodachi/Controllers/DojodachiController.cs
@@ -8,7 +8,7 @@ namespace Dojodachi.Controllers     //be sure to use your own project's namespac
     public class DojodachiController : Controller
     {
         [HttpGet("")] 
-        public ViewResult Index()
+        public IActionResult Index()
         {
             TempData["GameStatus"] = "playing";
             int? Happiness = HttpContext.Session.GetInt32("Happiness");
@@ -16,7 +16,60 @@ namespace Dojodachi.Controllers     //be sure to use your own project's namespac
             {
                 HttpContext.Session.SetInt32("Happiness", 20);
             }
-            return View();
+            int? Fullness = HttpContext.Session.GetInt32("Fullness");
+            if(Fullness == null)
+            {
+                HttpContext.Session.SetInt32("Fullness", 20);
+            }
+            int? Energy = HttpContext.Session.GetInt32("Energy");
+            if(Energy == null)
+            {
+                HttpContext.Session.SetInt32("Energy", 50);
+            }
+            int? Meals = HttpContext.Session.GetInt32("Meals");
+            if(Meals == null)
+            {
+                HttpContext.Session.SetInt32("Meals", 3);
+            }
+            TempData["Happiness"] = HttpContext.Session.GetInt32("Happiness");
+            TempData["Fullness"] = HttpContext.Session.GetInt32("Fullness");
+            TempData["Energy"] = HttpContext.Session.GetInt32("Energy");
+            TempData["Meals"] = HttpContext.Session.GetInt32("Meals");
+            if(Energy <= 0 || Happiness <= 0)
+            {
+                TempData["GameStatus"] = "over";
+                TempData["Message"] = "You Lost, Game Over";
+            }
+            if(Energy >= 100 && Fullness >= 100 && Happiness >= 100)
+            {
+                TempData["GameStatus"] = "over";
+                TempData["Message"] = "You Won! Get your Prize!";
+            }
+            return View("Index");
+        }
+
+        [HttpGet("feed")]
+        public IActionResult Feed()
+        {
+            if(HttpContext.Session.GetInt32("Meals") <= 0)
+            {
+                TempData["Message"] = "You Cant Feed your pet anymore";
+            }
+            else{
+                int? MealsData = HttpContext.Session.GetInt32("Meals") - 1;
+                HttpContext.Session.SetInt32("Meals", (int)MealsData);
+                Random rand = new Random();
+                int chance = rand.Next(0, 4);
+                if(chance == 0)
+                {
+                    TempData["Message"] = "You Pet didnt like its food";
+                }
+                else
+                {
+                    
+                }
+            }
+            return RedirectToAction("Index");
         }
     }
 }
