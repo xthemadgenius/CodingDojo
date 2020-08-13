817a979c4f674c148bc5694798666d5b71be2a27
xthemadgenius <javierjcjr@gmail.com>
2020-08-13 09:58:14 -0700

    more Code

--- C#/ORM/ChefsNDishes/Models/Dish.cs
+++ C#/ORM/ChefsNDishes/Models/Dish.cs
@@ -1,37 +1,35 @@
 using System.ComponentModel.DataAnnotations;
 using System;
 
-    namespace ChefsNDishes.Models
+namespace ChefsNDishes.Models
+{
+    public class Dish
     {
-        public class Dish
-        {
-            [Key]
-            public int DishId { get; set; }
+        [Key]
+        public int DishId { get; set; }
 
-            [Required]
-            [Display(Name = "Dish Name:")]
-            public string Name { get; set; }
+        [Required]
+        [Display(Name = "Dish Name:")]
+        public string Name { get; set; }
 
-            [Required]
-            [Range(0, 10000000)]
-            [Display(Name = "# of Calories:")]
-            public int Calories { get; set; }
+        [Required]
+        [Range(0, 10000000)]
+        [Display(Name = "# of Calories:")]
+        public int Calories { get; set; }
 
-            [Required]
-            [Range(0, 5)]
-            [Display(Name = "Tastiness Rating:")]
-            public int Tastiness { get; set; }
+        [Required]
+        [Range(0, 5)]
+        [Display(Name = "Tastiness Rating:")]
+        public int Tastiness { get; set; }
 
-            [Required]
-            [Display(Name = "Description:")]
-            public string Description { get; set; }
+        [Required]
+        [Display(Name = "Description:")]
+        public string Description { get; set; }
 
-            public int ChefId {get;set;}
-
-            public Chef Preparer {get; set;}
-
-
-            public DateTime CreatedAt {get;set;} = DateTime.Now;
-            public DateTime UpdatedAt {get;set;} = DateTime.Now;
-        }
+        public int ChefId {get;set;}
+        public Chef Preparer {get; set;}
+        
+        public DateTime CreatedAt {get;set;} = DateTime.Now;
+        public DateTime UpdatedAt {get;set;} = DateTime.Now;
     }
+}
