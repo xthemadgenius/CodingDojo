b82437d67a47deb257e638a69cca0509a230ff0a
xthemadgenius <javierjcjr@gmail.com>
2020-08-18 10:35:40 -0700

    got Login WOrking

--- C#/ORM/TheWall/Controllers/WallController.cs
+++ C#/ORM/TheWall/Controllers/WallController.cs
@@ -1,39 +1,90 @@
- using System;
-    using System.Linq;
-    using System.Collections.Generic;
-    using Microsoft.AspNetCore.Mvc;
-    using Microsoft.AspNetCore.Http;
-    using Microsoft.EntityFrameworkCore;
-    using TheWall.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using System.Linq;
+using TheWall.Models;
+using Microsoft.AspNetCore.Http;
 
-    namespace TheWall.Controllers
+namespace TheWall.Controllers
+{
+    public class WallController : Controller
     {
-        public class WallController : Controller
+        private MyContext _context;
+
+        public WallController(MyContext context)
         {
-            private MyContext _context;
+            _context = context;
+        }
 
-            public WallController(MyContext context)
-            {
-                _context = context;
-            }
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
 
-            [HttpGet("")]
-            public IActionResult Index()
+        [HttpPost("register")]
+        public IActionResult Register(LogRegWrapper RegForm)
+        {
+            if(ModelState.IsValid)
             {
-                return View("Index");
-            }
-
-            [HttpGet("wall")]
-            public IActionResult TheWall()
-            {
+                if(_context.users.Any(u => u.Email == RegForm.Register.Email))
+                {
+                    ModelState.AddModelError("Register.Email", "Try a different Email, Account already registered");
+                    return Index();
+                }
+                PasswordHasher<User> Hasher = new PasswordHasher<User>();
+                RegForm.Register.Password = Hasher.HashPassword(RegForm.Register, RegForm.Register.Password);
+                _context.Add(RegForm.Register);
+                _context.SaveChanges();
+                HttpContext.Session.SetInt32("UserId",  RegForm.Register.UserId);
                 return View("TheWall");
             }
+            return View("Index");
+        }
 
-            [HttpGet("logout")]
-            public IActionResult Logout()
+        [HttpPost("login")]
+        public IActionResult Login(LogRegWrapper LogForm)
+        {
+            if(ModelState.IsValid)
             {
-                HttpContext.Session.Clear();
-                return Redirect("/");
+                User db_user = _context.users.FirstOrDefault(u => u.Email == LogForm.Login.Email);
+                if(db_user == null)
+                {
+                    ModelState.AddModelError("Login.Email", "Try a different Email, Email not valid");
+                    return Index();
+                }
+                PasswordHasher<LoginUser> Hasher = new PasswordHasher<LoginUser>();
+                PasswordVerificationResult Result = Hasher.VerifyHashedPassword(LogForm.Login, db_user.Password, LogForm.Login.Password);
+                if(Result == 0)
+                {
+                    ModelState.AddModelError("Login.Email", "Invalid Email, Try another one");
+                    return Index();
+                }
+                HttpContext.Session.SetInt32("UserId",  db_user.UserId);
+                return Redirect("/dashboard");
             }
+            return View("Index");
+        }
+
+        [HttpGet("dashboard")]
+        public IActionResult TheWall()
+        {
+            int? loggedUser = HttpContext.Session.GetInt32("UserId");
+            if(loggedUser == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View("TheWall");
+        }
+
+        [HttpGet("logout")]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return Redirect("/");
         }
     }
+}
---
+++ C#/ORM/TheWall/Migrations/20200818172553_FirstMigration.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TheWall.Migrations
+{
+    public partial class FirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(nullable: false),
+                    LastName = table.Column<string>(nullable: false),
+                    Email = table.Column<string>(nullable: false),
+                    Password = table.Column<string>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "messages",
+                columns: table => new
+                {
+                    MessageId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    MsgContent = table.Column<string>(nullable: true),
+                    UserId = table.Column<int>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_messages", x => x.MessageId);
+                    table.ForeignKey(
+                        name: "FK_messages_users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "comments",
+                columns: table => new
+                {
+                    CommentId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ComContent = table.Column<string>(nullable: true),
+                    UserId = table.Column<int>(nullable: false),
+                    MessageId = table.Column<int>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_comments", x => x.CommentId);
+                    table.ForeignKey(
+                        name: "FK_comments_messages_MessageId",
+                        column: x => x.MessageId,
+                        principalTable: "messages",
+                        principalColumn: "MessageId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_comments_users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_comments_MessageId",
+                table: "comments",
+                column: "MessageId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_comments_UserId",
+                table: "comments",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_messages_UserId",
+                table: "messages",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "comments");
+
+            migrationBuilder.DropTable(
+                name: "messages");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
