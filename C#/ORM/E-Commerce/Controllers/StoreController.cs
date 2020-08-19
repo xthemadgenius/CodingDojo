58a79a881570a75119a8e677ab0feaaa0286e219
xthemadgenius <javierjcjr@gmail.com>
2020-08-19 00:53:23 -0700

    Mostly operational E-Com Missing Delete user & Main Filter on Dash n Prod

--- C#/ORM/E-Commerce/Controllers/StoreController.cs
+++ C#/ORM/E-Commerce/Controllers/StoreController.cs
@@ -22,24 +22,85 @@ namespace E_Commerce.Controllers
         [HttpGet("")]
         public IActionResult Dashboard()
         {
-            return View("Dashboard");
+            DashWrapper wrap = new DashWrapper()
+            {
+                AllProducts = _context.Products.Take(5).ToList(),
+                RecentOrders = _context.Orders.Include(b => b.Buyer).Include(i => i.ItemBought).OrderByDescending(o => o.CreatedAt).ToList(),
+                NewCustomers = _context.Customers.OrderByDescending(c => c.CreatedAt).Take(3).ToList(),
+            };
+            return View("Dashboard", wrap);
         }
 
         [HttpGet("products")]
         public IActionResult Products()
         {
+            ProdWrapper wrap = new ProdWrapper();
+            wrap.AllProducts = _context.Products.ToList();
+            return View("Products", wrap);
+        }
+
+        [HttpPost("products/new")]
+        public IActionResult Items(ProdWrapper item)
+        {
+            if(ModelState.IsValid){
+                _context.Add(item.AddProduct);
+                _context.SaveChanges();
+                return RedirectToAction("Products");
+            }
             return View("Products");
         }
 
         [HttpGet("orders")]
         public IActionResult Orders()
         {
+            ShopperWrapper wrap = new ShopperWrapper()
+            {
+                AllOrders = _context.Orders.OrderByDescending(c => c.CreatedAt)
+                                    .Include(b => b.Buyer)
+                                    .Include(i => i.ItemBought)
+                                    .ToList(),
+                AllCustomers = _context.Customers.ToList(),
+                AllProducts = _context.Products.ToList()
+            };
+            return View("Orders", wrap);
+        }
+
+        [HttpPost("orders/new")]
+        public IActionResult Purchases(ShopperWrapper purchase)
+        {
+            if(ModelState.IsValid)
+            {
+                var PickProd = _context.Products
+                            .FirstOrDefault(p => p.ProductId == purchase.AddOrder.ProductId);
+                PickProd.Quantity -= purchase.AddOrder.Quantity;
+                PickProd.UpdatedAt = DateTime.Now;
+                _context.Add(purchase.AddOrder);
+                _context.Products.Update(PickProd);
+                _context.SaveChanges();
+                return RedirectToAction("Orders", PickProd);
+            }
             return View("Orders");
         }
 
         [HttpGet("customers")]
         public IActionResult Customers()
         {
+            CustomerWrapper wrap = new CustomerWrapper();
+            wrap.AllCustomers = _context.Customers
+                            .OrderByDescending(c => c.CreatedAt)
+                            .ToList();
+            return View("Customers", wrap);
+        }
+
+        [HttpPost("customers/new")]
+        public IActionResult Shopper(CustomerWrapper shopper)
+        {
+            if(ModelState.IsValid){
+                CustomerWrapper wrap = new CustomerWrapper();
+                _context.Add(shopper.NewCustomer);
+                _context.SaveChanges();
+                return RedirectToAction("Customers", wrap);
+            }
             return View("Customers");
         }
 
